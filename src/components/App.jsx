fe34cdfa10a4d58d2c9d90861e6f4585e542f3b9
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-20 19:51:18 -0400

    added main app component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import RouteSwitch from "./RouteSwitch";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <RouteSwitch />
+    </BrowserRouter>
+  );
+};
+
+export default App;
