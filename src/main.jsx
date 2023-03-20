2d56612a0105bd92294b497159386506d4573be3
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-20 19:52:43 -0400

    properly imported chakra with react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
-import Home from "./routes/Home";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Home />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
