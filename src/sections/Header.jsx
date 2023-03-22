31facf63900c0fb901b27332da4b82ab26f3995a
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-22 16:32:46 -0400

    added bottom border

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,28 +1,45 @@
 import React from "react";
-import { Flex, HStack, Heading, Spacer, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, HStack, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ThemeSwitcher from "../utils/ThemeSwitcher"
 
 const Header = () => {
     return (
-        <Flex w="100%" justifyContent="center" h="75px" alignItems="center">
-            <Flex w="80%" gap={6} justifyContent="center" alignItems="center">
-                <Heading cursor="default">
-                    <a href="/">
-                        <Text color={useColorModeValue("pink.500", "pink.100")}>keebstore</Text>
-                    </a>
+        <Flex
+            w="100%"
+            justifyContent="center"
+            h="75px"
+            alignItems="center"
+            borderBottom="4px solid"
+            borderColor={useColorModeValue("pink.400", "pink.100")}
+        >
+            <Flex
+                w="80%"
+                gap={6}
+                justifyContent="space-between"
+                alignItems="center"
+                h="100%"
+            >
+                <Heading
+                    cursor="default"
+                    color={useColorModeValue("pink.400", "pink.100")}
+                >
+                    <Text >
+                        <a href="/">
+                            keebstore
+                        </a>
+                    </Text>
                 </Heading>
-                <Spacer />
+                <Text><a href="/products">shop</a></Text>
                 <HStack mr={17} spacing={5}>
-                    <a href="/products">shop</a>
                     <a href="/cart">
                         <FontAwesomeIcon icon={faCartShopping} />
                     </a>
+                    <ThemeSwitcher />
                 </HStack>
-                <ThemeSwitcher />
             </Flex>
-        </Flex>
+        </Flex >
     );
 };
 
