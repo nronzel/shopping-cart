e7d725d2e6abe21f9b9112bc037164cb1eed55c5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-21 21:52:15 -0400

    added config

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from "./components/App";
 import theme from "./theme"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
 );
