a1ca88739a3a2bf69ad8e8fef3b9256446759500
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-22 19:04:27 -0400

    added categorycards

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,19 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import HomepageHero from "../components/HomepageHero";
+import CategoryCards from "../components/CategoryCards";
 
 const Home = () => {
     return (
         <Flex direction="column">
             <HomepageHero />
-            <Outlet />
+            <Flex>
+                <CategoryCards imgSource="/categories/switches.jpg" />
+                <CategoryCards imgSource="/categories/chasis.jpg" />
+                <CategoryCards imgSource="/categories/keycaps.jpg" />
+                <CategoryCards imgSource="/categories/cables.jpg" />
+                <Outlet />
+            </Flex>
         </Flex>
     );
 };
