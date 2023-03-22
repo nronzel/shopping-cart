3424d114a3d5e12b20917b37a298871078e5261e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-22 19:42:41 -0400

    made slightly larger

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,7 +4,12 @@ import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
 const Footer = () => {
     return (
-        <Flex h="50px" justifyContent="center" alignItems="center">
+        <Flex
+            h="70px"
+            justifyContent="center"
+            alignItems="center"
+            flexShrink="0"
+        >
             <Text
                 color={useColorModeValue("gray.300", "gray.600")}>
                 nronzel &copy; 2023 All Rights Reserved
