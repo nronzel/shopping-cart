092c6250d1e9d2d8ae9acd207eafa69d7b803d3b
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-22 21:04:19 -0400

    set bottom "border" to be gradient

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,44 +1,57 @@
 import React from "react";
-import { Flex, HStack, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, HStack, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ThemeSwitcher from "../utils/ThemeSwitcher"
 
 const Header = () => {
+    const gradientBg = "linear-gradient(90deg, rgba(227,68,227,1) 0%, rgba(253,29,29,1) 100%)"
+
+    const borderStyle = {
+        backgroundImage: gradientBg
+    };
+
     return (
         <Flex
             w="100%"
             justifyContent="center"
             h="75px"
-            alignItems="center"
-            borderBottom="4px solid"
-            borderColor={useColorModeValue("pink.400", "pink.100")}
+            alignItems="flex-start"
+            bgGradient={gradientBg}
             flexShrink="0"
+            zIndex="0"
         >
             <Flex
-                w="80%"
-                gap={6}
-                justifyContent="space-between"
-                alignItems="center"
-                h="100%"
+                w="100%"
+                bgColor={useColorModeValue("gray.100", "gray.800")}
+                h="71px"
+                justifyContent="center"
+                zIndex="1"
             >
-                <Heading
-                    cursor="default"
-                    bgClip="text"
-                    bgGradient="linear-gradient(90deg, rgba(227,68,227,1) 0%, rgba(253,29,29,1) 100%)"
-                    fontSize="4xl"
+                <Flex
+                    w="80%"
+                    gap={6}
+                    justifyContent="space-between"
+                    alignItems="center"
                 >
-                    <a href="/">
-                        keebstore
-                    </a>
-                </Heading>
-                <Text fontSize="md"><a href="/products">shop</a></Text>
-                <HStack mr={17} spacing={5}>
-                    <a href="/cart">
-                        <FontAwesomeIcon icon={faCartShopping} />
-                    </a>
-                    <ThemeSwitcher />
-                </HStack>
+                    <Heading
+                        cursor="default"
+                        bgClip="text"
+                        bgGradient={gradientBg}
+                        fontSize="4xl"
+                    >
+                        <a href="/">
+                            keebstore
+                        </a>
+                    </Heading>
+                    <Text fontSize="md"><a href="/products">shop</a></Text>
+                    <HStack mr={17} spacing={5}>
+                        <a href="/cart">
+                            <FontAwesomeIcon icon={faCartShopping} />
+                        </a>
+                        <ThemeSwitcher />
+                    </HStack>
+                </Flex>
             </Flex>
         </Flex >
     );
