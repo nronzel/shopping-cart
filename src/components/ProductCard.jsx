2439354abe0f7878c8df7af32815c3810215940e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-24 13:19:39 -0400

    component for product cards

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Heading,
+  Divider,
+  ButtonGroup,
+  Button,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React from "react";
+
+const ProductCard = ({ productTitle, imgSource, price }) => {
+  return (
+    <Card maxW="xs" borderRadius="xl" bgColor={useColorModeValue("gray.100", "gray.700")}>
+      <CardBody>
+        <Image
+          h="250px"
+          w="100%"
+          objectFit="cover"
+          objectPosition="center"
+          src={imgSource}
+          borderRadius="lg"
+          alt="product image"
+        />
+        <Stack spacing={3} mt={4}>
+          <Heading size="md">{productTitle}</Heading>
+          <Text fontWeight="thin">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor in
+            dolorem fugit quo obcaecati facere!
+          </Text>
+          <Text
+            color={useColorModeValue("pink.500", "pink.200")}
+            fontSize="lg"
+            fontWeight="bold"
+          >
+            {price}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup>
+          <Button variant="solid" colorScheme="pink">Add to cart</Button>
+          <Button variant="outline" fontWeight="normal">
+            Details
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProductCard;
