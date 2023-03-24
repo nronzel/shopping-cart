43425f0443deccfba9627bfdf7ea43028c3a7693
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-23 23:05:56 -0400

    prettierd

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
-
 const Footer = () => {
-    return (
-        <Flex
-            h="70px"
-            justifyContent="center"
-            alignItems="center"
-            flexShrink="0"
-        >
-            <Text
-                color={useColorModeValue("gray.300", "gray.600")}>
-                nronzel &copy; 2023 All Rights Reserved
-            </Text>
-        </Flex>
-    );
+  return (
+    <Flex h="70px" justifyContent="center" alignItems="center" flexShrink="0">
+      <Text color={useColorModeValue("gray.300", "gray.600")}>
+        nronzel &copy; 2023 All Rights Reserved
+      </Text>
+    </Flex>
+  );
 };
 
 export default Footer;
--- src/sections/Layout.jsx
+++ src/sections/Layout.jsx
@@ -5,14 +5,14 @@ import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <Flex direction="column" alignItems="center" h="100vh">
-            <Header />
-            <Outlet />
-            <Spacer />
-            <Footer />
-        </Flex>
-    );
+  return (
+    <Flex direction="column" alignItems="center" h="100vh">
+      <Header />
+      <Outlet />
+      <Spacer />
+      <Footer />
+    </Flex>
+  );
 };
 
 export default Layout;
