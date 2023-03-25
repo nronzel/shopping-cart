51fce2065a3216e713bfb7ed6b86fde4add15ca2
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-25 17:24:21 -0400

    separated product data

---
+++ src/data/products.js
@@ -0,0 +1,13 @@
+const products = [
+  { name: "Product 1", price: "75", src: "/products/product1.jpg" },
+  { name: "Product 2", price: "150", src: "/products/product2.jpg" },
+  { name: "Product 3", price: "135", src: "/products/product3.jpg" },
+  { name: "Product 4", price: "80", src: "/products/product4.jpg" },
+  { name: "Product 5", price: "350", src: "/products/product5.jpg" },
+  { name: "Product 6", price: "35", src: "/products/product6.jpg" },
+  { name: "Product 7", price: "280", src: "/products/product7.jpg" },
+  { name: "Product 8", price: "275", src: "/products/product8.jpg" },
+  { name: "Product 9", price: "69", src: "/products/product9.jpg" },
+];
+
+export default products;
