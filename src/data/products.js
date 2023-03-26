cf6c37305840ae75decc9b17f92022287fa16525
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-26 13:44:23 -0400

    added id to data

--- src/data/products.js
+++ src/data/products.js
@@ -1,13 +1,13 @@
 const products = [
-  { name: "Product 1", price: "75", src: "/products/product1.jpg" },
-  { name: "Product 2", price: "150", src: "/products/product2.jpg" },
-  { name: "Product 3", price: "135", src: "/products/product3.jpg" },
-  { name: "Product 4", price: "80", src: "/products/product4.jpg" },
-  { name: "Product 5", price: "350", src: "/products/product5.jpg" },
-  { name: "Product 6", price: "35", src: "/products/product6.jpg" },
-  { name: "Product 7", price: "280", src: "/products/product7.jpg" },
-  { name: "Product 8", price: "275", src: "/products/product8.jpg" },
-  { name: "Product 9", price: "69", src: "/products/product9.jpg" },
+  { id: 0, name: "Product 1", price: 75, src: "/products/product1.jpg" },
+  { id: 1, name: "Product 2", price: 150, src: "/products/product2.jpg" },
+  { id: 2, name: "Product 3", price: 135, src: "/products/product3.jpg" },
+  { id: 3, name: "Product 4", price: 80, src: "/products/product4.jpg" },
+  { id: 4, name: "Product 5", price: 350, src: "/products/product5.jpg" },
+  { id: 5, name: "Product 6", price: 35, src: "/products/product6.jpg" },
+  { id: 6, name: "Product 7", price: 280, src: "/products/product7.jpg" },
+  { id: 7, name: "Product 8", price: 275, src: "/products/product8.jpg" },
+  { id: 8, name: "Product 9", price: 69, src: "/products/product9.jpg" },
 ];
 
 export default products;
