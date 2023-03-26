6c0b77635ef625ca1c72652c8b1798c292e6031b
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-26 13:44:01 -0400

    modified routes

--- src/components/RouteSwitch.jsx
+++ src/components/RouteSwitch.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../routes/Home";
-import Layout from "../sections/Layout";
-import Cart from "../routes/Cart";
 import Products from "../routes/Products";
 
 export default function RouteSwitch() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/products" element={<Products />} />
-      </Route>
+      <Route path="/" index element={<Home />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
