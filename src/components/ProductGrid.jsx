2b74100736971bc148e5033242cf090fabe7fb6e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-26 13:45:23 -0400

    modified props

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -3,19 +3,18 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import products from "../data/products";
 
-const ProductGrid = () => {
+const ProductGrid = ({ addToCart }) => {
   return (
     <Box maxW="1200px">
       <Heading mt={3} textAlign="center">
         all products
       </Heading>
       <Flex wrap="wrap" gap={5} mt={5} w="100%" justifyContent="center">
-        {products.map((product, index) => (
+        {products.map((product) => (
           <ProductCard
-            price={`$ ${product.price}`}
-            imgSource={product.src}
-            key={index}
-            productTitle={product.name}
+            product={product}
+            key={product.id}
+            addToCart={addToCart}
             flexGrow="0"
           />
         ))}
