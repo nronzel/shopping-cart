291e1bab7d2a17d05fccd4b88e924a049bc4a31d
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-26 14:41:40 -0400

    modified cart view Layout

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -11,29 +11,46 @@ import React from "react";
 
 const CartView = ({ item, onQuantityChange, removeFromCart }) => {
   return (
-    <Card direction="row" alignItems="center">
-      <Image boxSize="70px" objectFit="cover" src={item.src} />
-      <Stack ml={5}>
-        <Text>{item.name}</Text>
-        <HStack>
+    <Card direction="row" alignItems="center" variant="outline">
+      <HStack w="100%" p={4}>
+        <Image
+          boxSize="65px"
+          objectFit="cover"
+          borderRadius={4}
+          src={item.src}
+        />
+        <Stack ml={5}>
+          <Text fontWeight="semibold">{item.name}</Text>
+          <HStack>
+            <Button
+              size="xs"
+              onClick={() => onQuantityChange(item.quantity - 1, item.id)}
+            >
+              -
+            </Button>
+            <Text>Qty: {item.quantity}</Text>
+            <Button
+              size="xs"
+              onClick={() => onQuantityChange(item.quantity + 1, item.id)}
+            >
+              +
+            </Button>
+          </HStack>
+        </Stack>
+        <Spacer />
+        <HStack gap={8}>
+          <Text fontWeight="bold" fontSize="lg">
+            $ {item.price * item.quantity}
+          </Text>
           <Button
+            colorScheme="pink"
             size="xs"
-            onClick={() => onQuantityChange(item.quantity - 1, item.id)}
+            onClick={() => removeFromCart(item.id)}
           >
-            -
-          </Button>
-          <Text>Qty: {item.quantity}</Text>
-          <Button
-            size="xs"
-            onClick={() => onQuantityChange(item.quantity + 1, item.id)}
-          >
-            +
+            remove
           </Button>
         </HStack>
-      </Stack>
-      <Spacer />
-      <Text>$ {item.price * item.quantity}</Text>
-      <Button size="xs" onClick={() => removeFromCart(item.id)}>remove</Button>
+      </HStack>
     </Card>
   );
 };
